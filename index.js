761627518cf165fb9d9f424069e646b211c8610a
Navin-nash <1923014@saec.ac.in>
2024-10-12 21:13:08 +0530

    Done

--- index.js
+++ index.js
@@ -1,17 +1,58 @@
 class SortedList {
-  constructor() {}
+  constructor() {
+    this.items = []; // Initialize items as an empty array
+    this.length = 0; // Initialize length to 0
+  }
 
-  add(item) {}
+  add(item) {
+    // Add the item to the array
+    this.items.push(item);
+    // Sort the array in ascending order
+    this.items.sort((a, b) => a - b);
+    // Update the length property
+    this.length = this.items.length;
+  }
 
-  get(pos) {}
+  get(pos) {
+    // Check if pos is out of bounds
+    if (pos < 0 || pos >= this.length) {
+      throw new Error('OutOfBounds');
+    }
+    // Return the item at the specified position
+    return this.items[pos];
+  }
 
-  max() {}
+  max() {
+    // Check if the items array is empty
+    if (this.length === 0) {
+      throw new Error('EmptySortedList');
+    }
+    // Return the maximum value
+    return this.items[this.length - 1];
+  }
 
-  min() {}
+  min() {
+    // Check if the items array is empty
+    if (this.length === 0) {
+      throw new Error('EmptySortedList');
+    }
+    // Return the minimum value
+    return this.items[0];
+  }
 
-  sum() {}
+  sum() {
+    // Return the sum of the items
+    return this.items.reduce((acc, curr) => acc + curr, 0);
+  }
 
-  avg() {}
+  avg() {
+    // Check if the items array is empty
+    if (this.length === 0) {
+      throw new Error('EmptySortedList');
+    }
+    // Return the average of the items
+    return this.sum() / this.length;
+  }
 }
 
 module.exports = SortedList;
